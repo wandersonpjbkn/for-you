b5c26c1787d191084861c5a09278f66437e2ad64
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2025-05-25 13:17:48 -0300

    Revert "chore: adds PWA feature"
    This reverts commit 3582beefc7f6bf0dcbe383b9bd1e07ed850d5ecb.

--- vite.config.ts
+++ vite.config.ts
@@ -3,38 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'I Got a Plant For You',
-        short_name: 'ForYou',
-        description: 'A simple reminder to help you have a more pleasant day',
-        theme_color: '#fe4c5d',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      devOptions: {
-        enabled: true
-      }
-    })
   ],
   resolve: {
     alias: {
